3747afc5318ec53a066469f489548219a3aa813b
ailishsmith <ailishsmith2021@gmail.com>
2025-05-19 20:44:49 -0700

    buttons

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,8 +8,24 @@
 
 // add button to challenge section
 $("#Challenge").append("<button id='button-challenge'>Make Special</button>");
+    // add a click listener to the challenge button
+    $("#button-challenge").click(function(){
+        // now add (or subtract) the "special" class to the section
+        $("#challenge").toggleClass("special");
+    });
+
 // add button to problems section
 $("#Problems").append("<button id='button-problems'>Make Special</button>");
+    // add a click listener to the problems button
+    $("#button-problems").click(function(){
+        // now add (or subtract) the "special" class to the section
+        $("#Problems").toggleClass("special");
+    });
+
 // add button to results section
 $("#Results").append("<button id='button-results'>Make Special</button>");
-
+    // add a click listener to the results button
+    $("#button-results").click(function(){
+        // now add (or subtract) the "special" class to the section
+        $("#Results").toggleClass("special");
+    });
