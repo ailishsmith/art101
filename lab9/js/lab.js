66313047a870831f313197c957c624d3dc6634ec
ailishsmith <ailishsmith2021@gmail.com>
2025-05-19 20:39:39 -0700

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,6 +1,15 @@
-// index.js - 
+// index.js - Lab 9: Libraries & jQuery
+    // lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+    // Requirements: jQuery must be loaded for this script to work.
 // Author: ailish smith aibsmith@ucsc.edu
-// Date: 
+// Date: 5/19/25
 
 
+
+// add button to challenge section
+$("#Challenge").append("<button id='button-challenge'>Make Special</button>");
+// add button to problems section
+$("#Problems").append("<button id='button-problems'>Make Special</button>");
+// add button to results section
+$("#Results").append("<button id='button-results'>Make Special</button>");
 
