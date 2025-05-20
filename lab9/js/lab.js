c4047c898376dc234f310001cb9e857c87589293
ailishsmith <ailishsmith2021@gmail.com>
2025-05-19 21:31:53 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -11,7 +11,7 @@ $("#Challenge").append("<button id='button-challenge'>Make Special</button>");
     // add a click listener to the challenge button
     $("#button-challenge").click(function(){
         // now add (or subtract) the "special" class to the section
-        $("#challenge").toggleClass("special");
+        $("#Challenge").toggleClass("special");
     });
 
 // add button to problems section
