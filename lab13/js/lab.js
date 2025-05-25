3efa99e13a6a37b1d41860bdb57eb6a6bdd7fa16
ailishsmith <ailishsmith2021@gmail.com>
2025-05-24 19:24:58 -0700

    rdsdkg

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -10,13 +10,16 @@ for (let i=1; i<=200; i++) {
     let str = "";
     // Add an if statement: if the number is a multiple of 3, print “Fizz” instead of the number.
     if (i % 3 == 0)
-        str += "fizz";
+      str += "fizz";
   // Add an else if condition: If the number is a multiple of 5, print “Buzz” instead of the number.
     if (i % 5 == 0)
       str += "buzz";
   // If the number is a multiple of 7, it should print "Boom!"
     if (i % 7 == 0)
       str += "boom";
+  // Can you add an additional factor?
+    if (i % 4 ==0)
+      str += "bang";
   // If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on.
 //formatting tips from ChatGPT
     if (str === "") {
